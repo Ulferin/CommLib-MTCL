abba32609aac2d45831a7aeb79b9def83747ab93
Nicolò Tonci <nicolo.tonci@phd.unipi.it>
2022-11-10 13:10:07 +0100

    Second commit

--- main.cpp
+++ main.cpp
@@ -1,35 +1,64 @@
 #include <iostream>
 #include <vector>
-#include <mpi.h>
+#include "mpi.h"
+
+
+
+class Handle {
+    virtual void send();
+    virtual void receive();
+    virtual void test();
+};
 
 class ConnType {
 
     virtual void init();
-    virtual void send();
-    virtual void receive();
-    virtual bool isReady();
+    virtual void* getReady();
+    virtual void manage(void* handle);
+    virtual void unmanage(void* handle);
+
 };
 
 class ConnTcp : public ConnType {
-    
-    bool isReady(){
-        if (isCLosed) return false;
+    // mutex per gestione del set (tutte le operazioni sul set)
+    // map tra descriptor e handle;
+    // set 
+    void* getReady(){
+        lock()
+        select(timeout);
+         // se nuova connessione aggiuni nella mappa e crea nuovo handle e modifica il set
+        unlock()
+        // select TCP
     }
 
-    Handle getHandle
+    void manage(void* h){
+        lock();
+        // aggiunge al set
+        unlock();
+    }
 };
 
+class HandleTCP : public Handle {
+    
+};
+
+
+
 class ConnMPI : public ConnType {
-    // rank - tag - comm
-    bool isReady(){
-        MPI_IProbe(/**/);
+    //map tra std::pair<std::atomic<bool>, tuple<int, int, int>> -> handle
+    // se
+    void*  getReady(){
+        // check nuove connessioni con IProbe(ANY_SOURCE, TAG_NUOVE_CONNESSIONI);
+
+        for(handle) IProbe 
+        //MPI_IProbe(/**/);
         // se pronto return true altrimenti false;
     }
 };
 
 
 class Manager {
-    std::vector<ConnType*> connections;
+    std::vector<ConnType*> protocols;
     void* nextReady;
 
     void* getReady(){
@@ -40,22 +69,49 @@ class Manager {
     }
 
     void getReadyBackend(){
-        for(auto& c : connections)
-            if (c.isReady())
-                nextReady = c;
-       
-
+        while(true){
+            for(auto& c : protocols){
+                auto* ready = c.getReady();
+                if (ready) nextReady = ready;
+            }
+            nanosleep(2000);
+        }
         // dormi per n ns
         // riprova da capo
     }
+
+    void connect(/* tipo connessione TCP:MPI, parametri connessione*/);
 };
 
 
+class Handle{
+    void yield(){
+        bool blocked = true;
+    
+       // comunica al manager che deve gestire questo handle (modifica il bool se mpi, aggiungi nel set se TCP)
+    } 
 
+    void send (){
+        if (blocked) throw err;
+    }
+}
 
 
 int main(int argc, char** argv){
-    manager.connect()// TCP : hostname:porta // MPI COMM_WORL:rank:tag
+    manager.registerProtocol<ConnTcp>("TCP");
+
+    auto handle = manager.connect<ConnTCP>("hostname:port");// TCP : hostname:porta // MPI COMM_WORL:rank:tag
+
+    handle.send()
+    handle.yield();
+    
+    handle = getReady();
+    if (handle){
+
+    }
+
+
+    handle.yield();
 
     return 0;
 }
