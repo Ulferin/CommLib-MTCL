3a50a2249fc2831d2a83bac95ba406a63ca04465
massimotorquati <massimo.torquati@unipi.it>
2022-12-23 14:37:48 +0000

    Deploying to gh-pages from @ nicolotonci/CommLib@5623861bc12d63af50fa88fe91022f003e68eceb 🚀

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['getnext_41',['getNext',['../classManager.html#a59a45bb53f976859df1eda9b0bf3c94d',1,'Manager']]],
+  ['getnext_41',['getNext',['../classManager.html#a13ef8962565fa814360e541e2aaa0d21',1,'Manager']]],
   ['gettypeofhandle_42',['getTypeOfHandle',['../classManager.html#a52731c2088e57556428ec505b44e1839',1,'Manager']]]
 ];
