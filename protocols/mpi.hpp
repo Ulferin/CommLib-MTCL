e95f48fb2c8404ee57d4218202542f222d1c34a2
Nicolò Tonci <nicolo.tonci@phd.unipi.it>
2023-01-09 11:28:21 +0100

    mpi first refactoring to support new termination protocol

--- protocols/mpi.hpp
+++ protocols/mpi.hpp
@@ -12,7 +12,10 @@
 
 #include "../handle.hpp"
 #include "../protocolInterface.hpp"
+#include "../utils.hpp"
+#include "../config.hpp"
 
+#define ECOMM 3
 
 class HandleMPI : public Handle {
 	
@@ -20,9 +23,9 @@ public:
     bool closing = false;
     int rank;
     int tag;
-    HandleMPI(ConnType* parent, int rank, int tag, bool busy=true): Handle(parent,busy), rank(rank), tag(tag){}
+    HandleMPI(ConnType* parent, int rank, int tag): Handle(parent), rank(rank), tag(tag){}
 	
-    ssize_t send(const void* buff, size_t size) {
+    /*ssize_t send(const void* buff, size_t size) {
         MPI_Request request;
         if (MPI_Isend(buff, size, MPI_BYTE, rank, tag, MPI_COMM_WORLD, &request) != MPI_SUCCESS){
 			MTCL_MPI_PRINT(100, "HandleMPI::send MPI_Isend ERROR\n");
@@ -43,9 +46,24 @@ public:
         }
 
         return size;
+    }*/
+
+    ssize_t send(const void* buff, size_t size) {
+        if (MPI_Send(&size, 1, MPI_UNSIGNED_LONG, this->rank, this->tag, MPI_COMM_WORLD) != MPI_SUCCESS){
+            MTCL_MPI_PRINT(100, "HandleMPI::send MPI_Send Header ERROR\n");
+            errno = ECOMM;
+            return -1;
+        }
+    
+        if (MPI_Send(buff, size, MPI_BYTE, this->rank, this->tag, MPI_COMM_WORLD) != MPI_SUCCESS){
+            MTCL_MPI_PRINT(100, "HandleMPI::send MPI_Send Payload ERROR\n");
+            errno = ECOMM;
+            return -1;
+        }
+        return size;
     }
 
-    ssize_t receive(void* buff, size_t size){
+    /*ssize_t receive(void* buff, size_t size){
         MPI_Status status; 
         int count;
         int flag = 0;
@@ -75,22 +93,44 @@ public:
 		}
 		MPI_Get_count(&status, MPI_BYTE, &count);
 		return count;
+    }*/
+
+    ssize_t receive(void* buff, size_t size){
+        int r;
+        MPI_Status s;
+        if (MPI_Recv(buff, size, MPI_BYTE, this->rank, this->tag, MPI_COMM_WORLD, &s) != MPI_SUCCESS){
+            MTCL_MPI_PRINT(100, "HandleMPI::receive MPI_Recv ERROR\n");
+			errno = ECOMM;
+			return -1;
+        }
+        MPI_Get_count(&s, MPI_BYTE, &r);
+        return r;
     }
+
+    ssize_t probe(size_t& size, const bool blocking=true){
+        if (MPI_Recv(&size, 1, MPI_UNSIGNED_LONG, this->rank, this->tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE) != MPI_SUCCESS){
+            MTCL_MPI_PRINT(100, "HandleMPI::probe MPI_Recv ERROR\n");
+			errno = ECOMM;
+            return -1;
+        }
+        return 0;
+    }
+
+    ssize_t sendEOS() {
+		size_t sz = 0;
+		return MPI_Send(&sz, 1, MPI_UNSIGNED_LONG, this->rank, this->tag, MPI_COMM_WORLD); 
+	}
 };
 
 
 
 class ConnMPI : public ConnType {
 protected:
-    // Supponiamo tutte le comunicazioni avvengano sul comm_world
-    // map è <rank, tag> ---> <free, handle object>
-    // nuova map è <hash_val> --> <free, <rank,tag>, handle object>
     // std::map<std::pair<int,int>, std::pair<bool, Handle<ConnMPI>*>> handles;
 
     int rank;
     //std::map<HandleMPI*, bool> connections;
     std::map<std::pair<int, int>, std::pair<HandleMPI*, bool>> connections;
-    // std::vector<HandleMPI*> connections;
 
     std::shared_mutex shm;
 
@@ -129,8 +169,8 @@ public:
         int rank = stoi(dest.substr(0, dest.find(":")));
         int tag = stoi(dest.substr(dest.find(":") + 1, dest.length()));
 
-        if (tag == MPI_CONNECTION_TAG || tag == MPI_DISCONNECT_TAG){
-			MTCL_MPI_PRINT(100, "ConnMPI::connect the connection tag must be greater than 1\n");
+        if (tag == MPI_CONNECTION_TAG){
+			MTCL_MPI_PRINT(100, "ConnMPI::connect the connection tag must be greater than 0\n");
 			errno = EINVAL;
             return nullptr;
         }
@@ -144,7 +184,7 @@ public:
 		}
 
         // creo l'handle
-        auto* handle = new HandleMPI(this, rank, tag, true);
+        HandleMPI* handle = new HandleMPI(this, rank, tag);
 		{
 			REMOVE_CODE_IF(std::unique_lock lock(shm));
 			connections.insert({{rank, tag}, {handle, false}});
@@ -176,13 +216,14 @@ public:
             
             int source = status.MPI_SOURCE;
             int source_tag = header[0];
-            HandleMPI* handle = new HandleMPI(this, source, source_tag, false);
+            HandleMPI* handle = new HandleMPI(this, source, source_tag);
             REMOVE_CODE_IF(ulock.lock());			
             connections.insert({{source, source_tag},{handle, false}});
             addinQ(true, handle);
             REMOVE_CODE_IF(ulock.unlock());
         }
 
+        /*
         if (MPI_Iprobe(MPI_ANY_SOURCE, MPI_DISCONNECT_TAG, MPI_COMM_WORLD, &flag, &status) != MPI_SUCCESS) {
 			MTCL_MPI_ERROR("ConnMPI::update: MPI_Iprobe ERROR (DISCONNECT)\n");
 			errno = ECOMM;
@@ -212,6 +253,7 @@ public:
 			}
             REMOVE_CODE_IF(ulock.unlock());
         }
+        */
 		
         REMOVE_CODE_IF(ulock.lock());
         for (auto& [rankTagPair, handlePair] : connections) {
@@ -232,8 +274,20 @@ public:
     }
 
 
-    void notify_close(Handle* h) {
+    void notify_close(Handle* h, bool close_wr=true, bool close_rd=true) {
         HandleMPI* hMPI = reinterpret_cast<HandleMPI*>(h);
+
+        if (close_wr){
+            hMPI->sendEOS();
+        }
+
+        if (close_rd){
+            REMOVE_CODE_IF(std::unique_lock l(shm));
+            connections.erase({hMPI->rank, hMPI->tag});
+        }
+
+
+        /*
         if (!hMPI->closing){
             if (MPI_Send(&hMPI->tag, 1, MPI_INT, hMPI->rank, MPI_DISCONNECT_TAG, MPI_COMM_WORLD) != MPI_SUCCESS) {
 				MTCL_MPI_ERROR("ConnMPI::notify_close: MPI_Send ERROR\n");
@@ -243,12 +297,21 @@ public:
 		{
 			REMOVE_CODE_IF(std::unique_lock l(shm));
 			connections.erase({hMPI->rank, hMPI->tag});
-		}
+		}*/
     }
 	
 
     void notify_yield(Handle* h) {
         HandleMPI* hMPI = reinterpret_cast<HandleMPI*>(h);
+        
+        {
+            REMOVE_CODE_IF(std::unique_lock l(shm));
+            auto it = connections.find({hMPI->rank, hMPI->tag});
+            if (it != connections.end())
+                it->second.second = true;
+        }
+        
+        /*
         if (hMPI->closing) {
             addinQ(false, h);
             return;
@@ -257,6 +320,7 @@ public:
 			REMOVE_CODE_IF(std::unique_lock l(shm));
 			connections[{hMPI->rank, hMPI->tag}].second = true;
 		}
+        */
     }
 
 
