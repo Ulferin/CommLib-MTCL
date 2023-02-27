c0c5e473a465158576818ee2f8a06312993e5c1f
massimotorquati <massimo.torquati@unipi.it>
2023-02-27 10:12:46 +0000

    Deploying to gh-pages from @ nicolotonci/CommLib@802c03fd6e3744c6bd8e213e945745e41f4a8097 🚀

--- search/all_3.js
+++ search/all_3.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['faningeneric_16',['FanInGeneric',['../classFanInGeneric.html',1,'']]],
   ['fanoutgeneric_17',['FanOutGeneric',['../classFanOutGeneric.html',1,'']]],
-  ['finalize_18',['finalize',['../classManager.html#a4ac2bbb9d72ac637066a079976eaf106',1,'Manager']]]
+  ['finalize_18',['finalize',['../classManager.html#a33e1eb5d1e2cf4d4d1b23361d30743e7',1,'Manager']]]
 ];
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['end_80',['end',['../classConnType.html#a4781ceaf1366e6d080092b60affb0a0e',1,'ConnType::end()'],['../classConnMPI.html#a6efbf65b9aa6ceac80bb03cebec2bdbb',1,'ConnMPI::end()'],['../classConnMPIP2P.html#a1e76ad80dd1e6f82f1123109917a3f21',1,'ConnMPIP2P::end()'],['../classConnMQTT.html#a263dd19033bbd220f10f83241740877a',1,'ConnMQTT::end()'],['../classConnSHM.html#a6d40ca5186ec3f625a8632c19135bb6a',1,'ConnSHM::end()'],['../classConnTcp.html#a20d0571ae4673f70372ef24b9b29332f',1,'ConnTcp::end()'],['../classConnUCX.html#ab003f8e2b51f6e4ea96f3829bab2d197',1,'ConnUCX::end()']]]
+  ['end_80',['end',['../classConnType.html#ab4d02890e5d4f6744fa4e5162c128c8f',1,'ConnType::end()'],['../classConnMPI.html#ad355372560506e7922c8ad33b255e9d1',1,'ConnMPI::end()'],['../classConnMPIP2P.html#a1ff693e9d91ca63bacd27506e63c90a3',1,'ConnMPIP2P::end()'],['../classConnMQTT.html#a219afe3847b8a005c74e18823e9cf038',1,'ConnMQTT::end()'],['../classConnSHM.html#aaa9b1d3cbf4b1c687dcd46fa703180ad',1,'ConnSHM::end()'],['../classConnTcp.html#a60265fe07fe059acfb764237c52b9446',1,'ConnTcp::end()'],['../classConnUCX.html#ac04d351e0d2aae07e475a9d4967f3aac',1,'ConnUCX::end()']]]
 ];
