2a80630836ad90fa84f9d2a4dbb4a732cbaf9a36
massimotorquati <massimo.torquati@unipi.it>
2023-03-08 10:16:13 +0000

    Deploying to gh-pages from @ nicolotonci/CommLib@54f53f35e0265401a746ba4a594fd78b35871c59 🚀

--- search/all_1.js
+++ search/all_1.js
@@ -3,7 +3,7 @@ var searchData=
   ['collectivecontext_4',['CollectiveContext',['../classCollectiveContext.html',1,'']]],
   ['collectiveimpl_5',['CollectiveImpl',['../classCollectiveImpl.html',1,'']]],
   ['communicationhandle_6',['CommunicationHandle',['../classCommunicationHandle.html',1,'']]],
-  ['connect_7',['connect',['../classConnUCX.html#ac1fd058663e15bc0f637a7746e06b8b0',1,'ConnUCX::connect()'],['../classConnTcp.html#a5f8a1eade7b8db288ff7ef77f0c86022',1,'ConnTcp::connect()'],['../classConnSHM.html#a9b86b5bafd83a5b7bc91bacc6c815e89',1,'ConnSHM::connect()'],['../classConnMQTT.html#a30249d1596ee1b98b61f251c82a118f8',1,'ConnMQTT::connect()'],['../classConnMPIP2P.html#a91b9f60c6977dd1f14bd6a4fad5780e3',1,'ConnMPIP2P::connect()'],['../classConnMPI.html#a23b38202b801f517051a95e76081d1e3',1,'ConnMPI::connect()'],['../classConnType.html#a6bdb335c3f00795a1e4d8b25ba3127f4',1,'ConnType::connect()'],['../classManager.html#a842ea4e15e1f144e25a447b6a9df8d85',1,'Manager::connect()']]],
+  ['connect_7',['connect',['../classConnUCX.html#aee5067a26c79e1a139a6ed1c3f873562',1,'ConnUCX::connect()'],['../classConnTcp.html#a021c85d3162ab15363758386e9219a61',1,'ConnTcp::connect()'],['../classConnSHM.html#a898796bca2e73acab2972caa6f4df939',1,'ConnSHM::connect()'],['../classConnMQTT.html#a2f2d223cea4899077b2f32829b3cbf41',1,'ConnMQTT::connect()'],['../classConnMPI.html#a75e8ecc1c358a3a54fe2bd9aef2702fa',1,'ConnMPI::connect()'],['../classConnType.html#ab9a535577652f5c25e9cd43c4fa8f205',1,'ConnType::connect()'],['../classManager.html#abca887603242833d007cf50f2961f057',1,'Manager::connect()']]],
   ['connmpi_8',['ConnMPI',['../classConnMPI.html',1,'']]],
   ['connmpip2p_9',['ConnMPIP2P',['../classConnMPIP2P.html',1,'']]],
   ['connmqtt_10',['ConnMQTT',['../classConnMQTT.html',1,'']]],
